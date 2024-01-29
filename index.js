6ea693d13f119d25d9ad8fdb0be487af55bfeeaa
Vicpixel <150858788+Vicpixel@users.noreply.github.com>
2024-01-29 12:42:09 -0400

    mod. imagen, ubicacion del h2 y parrafo
    Modificaciones al codigo para que la imagen desaparesca al encriptar o desencriptar. Tambien se mueve el h2 y parrafo a la parte superior de la seccion encriptado al encriptar o desencriptar.
    Aumento del font size al recibir resultados.

--- index.js
+++ index.js
@@ -1,57 +1,96 @@
+let aumentoTamano = false; // Variable para rastrear si el tamaño ya se ha aumentado
+
 function encriptar() {
-  const texto = document.getElementById("texto").value;
-  const tituloMensaje = document.getElementById("titulo-mensaje");
-  const parrafo = document.getElementById("parrafo");
-  const muneco = document.getElementById("muneco");
-  const mensajeEncriptado = document.getElementById("mensaje-encriptado");
+    const texto = document.getElementById("texto").value;
+    const tituloMensaje = document.getElementById("titulo-mensaje");
+    const parrafo = document.getElementById("parrafo");
+    const muneco = document.getElementById("muneco");
+    const mensajeEncriptado = document.getElementById("mensaje-encriptado");
+    const btnCopiar = document.querySelector(".btn-copiar"); // Selecciona el botón de copiar
 
-  const textoCifrado = texto
-      .replace(/e/gi, 'enter')
-      .replace(/i/gi, 'imes')
-      .replace(/a/gi, 'ai')
-      .replace(/o/gi, 'ober');
+    const textoCifrado = texto
+        .replace(/e/gi, 'enter')
+        .replace(/i/gi, 'imes')
+        .replace(/a/gi, 'ai')
+        .replace(/o/gi, 'ober');
 
-  if (texto.length !== 0) {
-      mensajeEncriptado.style.visibility = "visible"; // Mostrar la sección encriptado
-      tituloMensaje.textContent = "Texto encriptado con éxito";
-      parrafo.textContent = "";
-      mensajeEncriptado.querySelector("#parrafo").textContent = textoCifrado; // Actualizar el texto encriptado
-  } else {
-      mensajeEncriptado.style.visibility = "hidden"; // Ocultar la sección encriptado
-      muneco.src = "./img/(1)Wall.E.png";
-      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
-      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-      swal("Oooopsss!", "Debes ingresar un texto", "warning");
-  }
+    if (texto.length !== 0) {
+        // Mueve el mensaje-encriptado a la parte superior
+        mensajeEncriptado.style.top = "0";
+        mensajeEncriptado.classList.add('show'); // Agrega la clase de animación
+        muneco.style.display = "none";
+
+        if (!aumentoTamano) {
+            // Incrementa el tamaño de la fuente del título y del párrafo una sola vez
+            tituloMensaje.style.fontSize = (parseInt(getComputedStyle(tituloMensaje).fontSize) + 5) + "px";
+            parrafo.style.fontSize = (parseInt(getComputedStyle(parrafo).fontSize) + 5) + "px";
+            aumentoTamano = true; // Marca que el tamaño ya se ha aumentado
+        }
+
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        mensajeEncriptado.querySelector("#parrafo").textContent = textoCifrado;
+        btnCopiar.style.display = "block";
+    } else {
+        // Restaura la posición original y el tamaño de la fuente
+        mensajeEncriptado.style.top = "75%";
+        mensajeEncriptado.style.visibility = "hidden";
+        muneco.style.display = "block";
+        tituloMensaje.style.fontSize = ""; // Restaura el tamaño de la fuente original
+        parrafo.style.fontSize = ""; // Restaura el tamaño de la fuente original
+        aumentoTamano = false; // Reinicia la variable para permitir el aumento en futuras operaciones
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Oooopsss!", "Debes ingresar un texto", "warning");
+        btnCopiar.style.display = "none";
+    }
 }
 
 function desencriptar() {
-  const texto = document.getElementById("texto").value;
-  const tituloMensaje = document.getElementById("titulo-mensaje");
-  const parrafo = document.getElementById("parrafo");
-  const muneco = document.getElementById("muneco");
-  const mensajeEncriptado = document.getElementById("mensaje-encriptado");
+    const texto = document.getElementById("texto").value;
+    const tituloMensaje = document.getElementById("titulo-mensaje");
+    const parrafo = document.getElementById("parrafo");
+    const muneco = document.getElementById("muneco");
+    const mensajeEncriptado = document.getElementById("mensaje-encriptado");
+    const btnCopiar = document.querySelector(".btn-copiar"); // Selecciona el botón de copiar
 
-  const textoCifrado = texto
-      .replace(/enter/gi, "e")
-      .replace(/imes/gi, "i")
-      .replace(/ai/gi, "a")
-      .replace(/ober/gi, "o")
-      .replace(/ufat/gi, "u");
+    const textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
 
-  if (texto.length !== 0) {
-      mensajeEncriptado.style.visibility = "visible"; // Mostrar la sección encriptado
-      muneco.src = "./img/muñeco.png";
-      tituloMensaje.textContent = "Texto desencriptado con éxito";
-      parrafo.textContent = "";
-      mensajeEncriptado.querySelector("#parrafo").textContent = textoCifrado; // Actualizar el texto desencriptado
-  } else {
-      mensajeEncriptado.style.visibility = "hidden"; // Ocultar la sección encriptado
-      muneco.src = "./img/muñeco.png";
-      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
-      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-      swal("Ooops!", "Debes ingresar un texto", "warning");
-  }
+    if (texto.length !== 0) {
+        // Mueve el mensaje-encriptado a la parte superior
+        mensajeEncriptado.style.top = "0";
+        mensajeEncriptado.classList.add('show'); // Agrega la clase de animación
+        muneco.style.display = "none";
+
+        if (!aumentoTamano) {
+            // Incrementa el tamaño de la fuente del título y del párrafo una sola vez
+            tituloMensaje.style.fontSize = (parseInt(getComputedStyle(tituloMensaje).fontSize) + 5) + "px";
+            parrafo.style.fontSize = (parseInt(getComputedStyle(parrafo).fontSize) + 5) + "px";
+            aumentoTamano = true; // Marca que el tamaño ya se ha aumentado
+        }
+
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        mensajeEncriptado.querySelector("#parrafo").textContent = textoCifrado;
+        btnCopiar.style.display = "block";
+    } else {
+        // Restaura la posición original y el tamaño de la fuente
+        mensajeEncriptado.style.top = "75%";
+        mensajeEncriptado.style.visibility = "hidden";
+        muneco.style.display = "block";
+        tituloMensaje.style.fontSize = ""; // Restaura el tamaño de la fuente original
+        parrafo.style.fontSize = ""; // Restaura el tamaño de la fuente original
+        aumentoTamano = false; // Reinicia la variable para permitir el aumento en futuras operaciones
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+        btnCopiar.style.display = "none";
+    }
 }
 
 function copiarResultado() {
@@ -67,5 +106,5 @@ function copiarResultado() {
 
     document.body.removeChild(textarea);
 
-    swal("Texto copiado al portapapeles");
+    alert("Texto copiado al portapapeles");
 }
